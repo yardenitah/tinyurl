6af4c2e1d925dbc9a4b6000241b3bcbbe65e9ee1
yarden itah <yardeit@ac.sce.ac.il>
2024-10-23 15:48:12 +0300

    commit with mongo

--- src/main/java/com/handson/tinyurl/controller/AppController.java
+++ src/main/java/com/handson/tinyurl/controller/AppController.java
@@ -3,13 +3,22 @@ package com.handson.tinyurl.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.handson.tinyurl.model.NewTinyRequest;
+import com.handson.tinyurl.model.User;
+import com.handson.tinyurl.repository.UserRepository;
 import com.handson.tinyurl.service.Redis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Random;
+
+import static com.handson.tinyurl.model.User.UserBuilder.anUser;
+import static com.handson.tinyurl.util.Dates.getCurMonth;
 
 @RestController
 public class AppController {
@@ -25,6 +34,32 @@ public class AppController {
     @Value("${base.url}")
     String baseUrl;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public User createUser(@RequestParam String name) {
+        User user = anUser().withName(name).build();
+        user = userRepository.insert(user);
+        return user;
+    }
+
+    @RequestMapping(value = "/user/{name}", method = RequestMethod.GET)
+    public User getUser(@RequestParam String name) {
+        User user = userRepository.findFirstByName(name);
+        return user;
+    }
+
+    private void incrementMongoField(String userName, String key) {
+        Query query = Query.query(Criteria.where("name").is(userName));
+        Update update = new Update().inc(key, 1);
+        mongoTemplate.updateFirst(query, update, "users");
+    }
+
+
     @RequestMapping(value = "/tiny", method = RequestMethod.POST)
     public String generate(@RequestBody NewTinyRequest request) throws JsonProcessingException {
         String tinyCode = generateTinyCode();
@@ -39,15 +74,22 @@ public class AppController {
 
     @RequestMapping(value = "/{tiny}/", method = RequestMethod.GET)
     public ModelAndView getTiny(@PathVariable String tiny) throws JsonProcessingException {
-        System.out.println("getRequest for tiny: " + tiny);
         Object tinyRequestStr = redis.get(tiny);
-        NewTinyRequest tinyRequest = om.readValue(tinyRequestStr.toString(),NewTinyRequest.class);
+        NewTinyRequest tinyRequest = om.readValue(tinyRequestStr.toString(), NewTinyRequest.class);
         if (tinyRequest.getLongUrl() != null) {
+            String userName = tinyRequest.getUserName();
+            if (userName != null) {
+                incrementMongoField(userName, "allUrlClicks");
+                incrementMongoField(userName,
+                        "shorts." + tiny + ".clicks." + getCurMonth());
+            }
             return new ModelAndView("redirect:" + tinyRequest.getLongUrl());
         } else {
             throw new RuntimeException(tiny + " not found");
         }
     }
+
+
     private String generateTinyCode() {
         String charPool = "ABCDEFHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         StringBuilder res = new StringBuilder();
@@ -57,23 +99,5 @@ public class AppController {
         return res.toString();
     }
 
-//    private String generateTinyCode() {
-//        StringBuilder res = new StringBuilder();
-//        for (int i = 0; i < TINY_LENGTH; i++) {
-//            int rand = random.nextInt(62);  // 26 (A-Z) + 26 (a-z) + 10 (0-9) = 62 possible characters
-//            if (rand < 26) {
-//                res.append((char) ('A' + rand));  // A-Z (65-90)
-//            } else if (rand < 52) {
-//                res.append((char) ('a' + rand - 26));  // a-z (97-122)
-//            } else {
-//                res.append((char) ('0' + rand - 52));  // 0-9 (48-57)
-//            }
-//        }
-//        return res.toString();
-//    }
-
-
-
 
 }
-
--- src/main/java/com/handson/tinyurl/model/NewTinyRequest.java
+++ src/main/java/com/handson/tinyurl/model/NewTinyRequest.java
@@ -3,6 +3,12 @@ package com.handson.tinyurl.model;
 public class NewTinyRequest {
     private  String longUrl;
 
+    private  String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
     public String getLongUrl() {
         return longUrl;
     }
